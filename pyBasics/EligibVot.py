6e70481e879b62b57a6c811cc1687edb6dccfb43
VaderXI <aronjoseph8008@gmail.com>
2025-07-01 22:37:09 +0530

    Add files via upload

---
+++ pyBasics/EligibVot.py
@@ -0,0 +1,7 @@
+age = int(input("Enter ur age: "))
+if (age>=18):
+    print("Eligible for voting")
+elif(age == 18):
+    print("Congrats. You are now eligible for voting")
+else:
+    print("Not eligible for voting")
